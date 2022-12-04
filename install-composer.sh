5daf55c7f59c79810e68a1b7640fdff360195d4b
Yunus Üstün <yunus.ustun@tubitak.gov.tr>
2022-12-04 20:03:03 +0300

    composer install script

---
+++ install-composer.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+sudo apt update
+sudo apt install -y php-cli unzip
+curl -sS https://getcomposer.org/installer -o /tmp/composer-setup.php
+sudo php /tmp/composer-setup.php --install-dir=/usr/local/bin --filename=composer
